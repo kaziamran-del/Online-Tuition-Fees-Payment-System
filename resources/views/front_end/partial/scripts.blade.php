32147114646b2ec382c338057563a34b657fb4c5
kazi amran <67230297+kaziamran-del@users.noreply.github.com>
2021-04-26 15:59:34 +0600

    Create scripts.blade.php

---
+++ resources/views/front_end/partial/scripts.blade.php
@@ -0,0 +1,12 @@
+<!-- js  code -->
+	<!-- library function -->
+	<script src="{{asset('front_end') }}/assets/js/jquery.js" type="text/javascript"></script>
+	<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+	<!-- Popper JS -->
+    <script src="{{asset('front_end') }}/assets/js/popper.min.js" type="text/javascript"></script>
+	<!-- Bootstrap JS -->
+	<script src="{{asset('front_end') }}/assets/js/bootstrap.min.js" type="text/javascript"></script>
+	<!-- Waypoint JS -->
+	<script src="{{asset('front_end') }}/assets/js/waypoint.min.js" type="text/javascript"></script>
+	<!-- main js -->
+	<script src="{{asset('front_end') }}/assets/js/main.js" type="text/javascript"></script>
